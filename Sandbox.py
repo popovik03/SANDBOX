0bacfa09b787182e3d74a7b7b59e1545d7b75176
Viktor <pedobonta@gmail.com>
2023-01-07 21:32:51 +0500

    проверка гита

---
+++ Sandbox.py
@@ -0,0 +1,15 @@
+print(int(3/2))
+print(float(2+2*2))
+
+
+car = "Toyota RAV4"
+print(car.replace("RAV4", "asshole"))
+my_list = [1,2,3,4,5]
+
+a = my_list * 40
+print(a)
+
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+a=matrix[1][2]
+print(a)
+#comment for git#
